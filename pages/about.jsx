bacfcaf9668e70bcb35cc503628c22b4c94499b5
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-06-04 14:58:13 +0200

    post ok

---
+++ pages/about.jsx
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </ul>
+
+        <h1 className="title">
+          Learn <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <h2>Mon about</h2>
+
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className="logo" />
+        </a>
+      </footer>
+
+
+    </div>
+  )
+}
